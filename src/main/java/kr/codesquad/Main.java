f735a710d7880148bd764ddfc254ae954754968a
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-02 16:40:32 +0900

    refactor: 메인 메서드 의존성 주입 형식 변경
    기존에는 argument전달 부분에서 직접 객체를 생성했는데, 이를 메서드로 분리함

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,13 +1,26 @@
 package kr.codesquad;
 
+import kr.codesquad.controller.LottoController;
 import kr.codesquad.controller.LottoControllerImpl;
 import kr.codesquad.lottomachine.LottoMachine;
 import kr.codesquad.lottomachine.LottoMachineImpl;
+import kr.codesquad.service.LottoService;
 import kr.codesquad.service.LottoServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        LottoMachine lottoMachine = new LottoMachineImpl(new LottoControllerImpl(), new LottoServiceImpl());
-        lottoMachine.start();
+        lottomachine().start();
+    }
+
+    private static LottoMachine lottomachine() {
+        return new LottoMachineImpl(lottoController(), lottoService());
+    }
+
+    private static LottoService lottoService() {
+        return new LottoServiceImpl();
+    }
+
+    private static LottoController lottoController() {
+        return new LottoControllerImpl();
     }
 }
