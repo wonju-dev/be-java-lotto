5f50edd79e1a074a9490933df6a58db870f5b464
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-02 16:33:37 +0900

    feat: 메인 메서드 구성

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,13 @@
 package kr.codesquad;
 
+import kr.codesquad.controller.LottoControllerImpl;
+import kr.codesquad.lottomachine.LottoMachine;
+import kr.codesquad.lottomachine.LottoMachineImpl;
+import kr.codesquad.service.LottoServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LottoMachine lottoMachine = new LottoMachineImpl(new LottoControllerImpl(), new LottoServiceImpl());
+        lottoMachine.start();
     }
 }
