1b35b4523fe60767fd9e6326bd1dd89b05579f5c
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-03 14:06:25 +0900

    refactor: Output 인터페이스 명세 변경
    기존에는 출력하려는 메시지의 종류, 타입에 따라 별도의 메서드를 지원하도록
    설게했는데, 오로지 출력 기능만 수행하도록 설계를 변경함

--- src/main/java/kr/codesquad/console/Output.java
+++ src/main/java/kr/codesquad/console/Output.java
@@ -1,14 +1,5 @@
 package kr.codesquad.console;
 
-import kr.codesquad.domain.Lotto;
-import kr.codesquad.domain.Result;
-
-import java.util.List;
-
 public interface Output {
     void print(String message);
-
-    void printLottos(List<Lotto> lottos);
-
-    void printResult(Result results);
 }
