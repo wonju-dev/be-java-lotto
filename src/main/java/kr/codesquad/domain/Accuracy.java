e4933313d0e611b03313f1a3c6e5df46b01fe269
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-03 15:01:33 +0900

    refactor: needBonus 필드 추가

--- src/main/java/kr/codesquad/domain/Accuracy.java
+++ src/main/java/kr/codesquad/domain/Accuracy.java
@@ -1,26 +1,42 @@
 package kr.codesquad.domain;
 
 public enum Accuracy {
-    SIX(6, 2000000000),
-    FIVE(5, 1500000),
-    FOUR(4, 50000),
-    THREE(3, 5000),
-    TWO(2, -1),
-    ONE(1, -1),
-    ZERO(0, -1);
-    private Integer match;
-    private Integer prize;
 
-    Accuracy(Integer match, Integer prize) {
+    SIX(6, false, 2000000000, "%d개 일치 (%d원)-%d개"),
+    FIVE_BONUS(5, true, 30000000, "%d개 일치, 보너스 볼 일치(%d원)-%d개"),
+    FIVE(5, false, 1500000, "%d개 일치 (%d원)-%d개"),
+    FOUR(4, false, 50000, "%d개 일치 (%d원)-%d개"),
+    THREE(3, false, 5000, "%d개 일치 (%d원)-%d개"),
+    TWO(2, false, -1, "%d개 일치 (%d원)-%d개"),
+    ONE(1, false, -1, "%d개 일치 (%d원)-%d개"),
+    ZERO(0, false, -1, "%d개 일치 (%d원)-%d개");
+
+    private Integer match;
+    private Boolean needBonus;
+    private Integer prize;
+    private String resultMessage;
+
+
+    Accuracy(Integer match, Boolean needBonus, Integer prize, String resultMessage) {
         this.match = match;
+        this.needBonus = needBonus;
         this.prize = prize;
+        this.resultMessage = resultMessage;
     }
 
     public Integer getMatch() {
-        return this.match;
+        return match;
     }
 
     public Integer getPrize() {
-        return this.prize;
+        return prize;
+    }
+
+    public Boolean needBonus() {
+        return needBonus;
+    }
+
+    public String getResultMessage() {
+        return resultMessage;
     }
 }
