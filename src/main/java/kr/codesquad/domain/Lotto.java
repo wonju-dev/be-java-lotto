eaad3d146b041862d17b8058154c30eb4790ea76
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-03 14:30:17 +0900

    refactor: 상수 위치 변경

--- src/main/java/kr/codesquad/domain/Lotto.java
+++ src/main/java/kr/codesquad/domain/Lotto.java
@@ -11,6 +11,8 @@ public class Lotto {
     private static final Integer RANGE_UPPER_BOUND = 46;
     private static final Integer COUNT_LOWER_BOUND = 0;
     private static final Integer COUNT_UPPER_BOUND = 6;
+    public static final Integer LOTTO_PRICE = 1000;
+
 
     private static List<Integer> randomNumbers = IntStream.range(RANGE_LOWER_BOUND, RANGE_UPPER_BOUND).boxed().collect(Collectors.toList());
 
