9134d63ffcd94f4803822d9d908a88830fe14913
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-03 14:13:26 +0900

    feat(Result): getNetProfit 기능 구현
    랜덤 생성된 로또와, 정답으로 입력된 값을 비교한 결과(수익)을 계산하는 기능을 구현함

--- src/main/java/kr/codesquad/domain/Result.java
+++ src/main/java/kr/codesquad/domain/Result.java
@@ -1,12 +1,15 @@
 package kr.codesquad.domain;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Result {
 
     private Map<Accuracy, Integer> scoreBoard;
 
     private Result() {
+        scoreBoard = new LinkedHashMap<>();
     }
 
     public static Result initResult() {
@@ -25,6 +28,19 @@ public class Result {
 
     public void record(Accuracy accuracy) {
         scoreBoard.put(accuracy, scoreBoard.get(accuracy) + 1);
+    }
+
+    public void removeUnnecessaryAccuracy() {
+        scoreBoard.remove(Accuracy.ZERO);
+        scoreBoard.remove(Accuracy.ONE);
+        scoreBoard.remove(Accuracy.TWO);
+    }
+
+    public Integer getNetProfit() {
+        Set<Map.Entry<Accuracy, Integer>> entries = scoreBoard.entrySet();
+        return entries.stream()
+                .map((Map.Entry<Accuracy, Integer> score) -> score.getKey().getPrize() * score.getValue())
+                .reduce(0, Integer::sum);
     }
 
     public Map<Accuracy, Integer> getScoreBoard() {
