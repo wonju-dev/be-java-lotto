4ad48d6de8a16c3d5f78225b4e30d5904466f82c
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-04 18:40:27 +0900

    feat(Accuracy): 출력 순서를 위해 Accuracy Comparator구현

--- src/main/java/kr/codesquad/enums/Accuracy.java
+++ src/main/java/kr/codesquad/enums/Accuracy.java
@@ -1,15 +1,14 @@
 package kr.codesquad.enums;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
+import java.util.Map;
 
 import static kr.codesquad.domain.MessageGenerator.BONUS_MATCH_RESULT_MSG_TEMPLATE;
 import static kr.codesquad.domain.MessageGenerator.MATCH_RESULT_MSG_TEMPLATE;
 
 public enum Accuracy {
-    ZERO(0, false, -1, MATCH_RESULT_MSG_TEMPLATE),
-    ONE(1, false, -1, MATCH_RESULT_MSG_TEMPLATE),
-    TWO(2, false, -1, MATCH_RESULT_MSG_TEMPLATE),
+    NO_MATCH(0, false, 0, MATCH_RESULT_MSG_TEMPLATE),
     THREE(3, false, 5000, MATCH_RESULT_MSG_TEMPLATE),
     FOUR(4, false, 50000, MATCH_RESULT_MSG_TEMPLATE),
     FIVE(5, false, 1500000, MATCH_RESULT_MSG_TEMPLATE),
@@ -20,12 +19,6 @@ public enum Accuracy {
     private Boolean needBonus;
     private Integer prize;
     private String resultMessage;
-
-    public static final List<Accuracy> REMOVE_LIST = List.of(
-            Accuracy.ZERO,
-            Accuracy.ONE,
-            Accuracy.TWO
-    );
 
     Accuracy(Integer match, Boolean needBonus, Integer prize, String resultMessage) {
         this.match = match;
@@ -40,7 +33,16 @@ public enum Accuracy {
                         (accuracy.needBonus && hasBonusNumber) || (!accuracy.needBonus && !hasBonusNumber)
                 ))
                 .findFirst()
-                .orElse(Accuracy.ZERO);
+                .orElse(Accuracy.NO_MATCH);
+    }
+
+    public static Comparator<? super Map.Entry<Accuracy, Integer>> getComparator() {
+        return new Comparator<Map.Entry<Accuracy, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Accuracy, Integer> o1, Map.Entry<Accuracy, Integer> o2) {
+                return o1.getKey().getMatch() - o2.getKey().getMatch();
+            }
+        };
     }
 
     public Integer getMatch() {
