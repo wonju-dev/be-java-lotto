c6291b59461b6f24526db6ecc07d2d0ce518df51
RentalHub <rentalhub@RentalHubui-MacBookPro-3.local>
2023-01-04 18:44:12 +0900

    refactor(LottoControllerImpl): 메서드 분리

--- src/main/java/kr/codesquad/controller/LottoControllerImpl.java
+++ src/main/java/kr/codesquad/controller/LottoControllerImpl.java
@@ -2,59 +2,65 @@ package kr.codesquad.controller;
 
 import kr.codesquad.console.Input;
 import kr.codesquad.console.Output;
-import kr.codesquad.domain.*;
+import kr.codesquad.domain.MessageGenerator;
+import kr.codesquad.domain.Result;
 import kr.codesquad.domain.lotto.AnswerLotto;
 import kr.codesquad.domain.lotto.Lotto;
-import kr.codesquad.domain.lottomachine.LottoMachine;
 import kr.codesquad.dto.PurchaseRecord;
-import kr.codesquad.domain.MessageGenerator;
+import kr.codesquad.service.LottoService;
 
 import java.util.List;
 
 public class LottoControllerImpl implements LottoController {
 
-    private final LottoMachine lottoMachine;
     private final Input input;
     private final Output output;
+    private final LottoService lottoService;
     private final MessageGenerator messageGenerator;
 
-    public LottoControllerImpl(LottoMachine lottoMachine, Input input, Output output, MessageGenerator messageGenerator) {
-        this.lottoMachine = lottoMachine;
+    public LottoControllerImpl(Input input, Output output, LottoService lottoService, MessageGenerator messageGenerator) {
         this.input = input;
         this.output = output;
+        this.lottoService = lottoService;
         this.messageGenerator = messageGenerator;
     }
 
     @Override
     public void start() {
-        PurchaseRecord purchaseRecord = getPurchaseRecord();
-        List<Lotto> lottos = lottoMachine.getRandomLottos(purchaseRecord.getCount());
-        output.print(messageGenerator.getLottoMessage(lottos));
+        PurchaseRecord purchaseRecord = makePurchaseRecord();
 
-        AnswerLotto answerLotto = getAnswerLotto();
-        Result results = lottoMachine.getResult(lottos, answerLotto);
+        List<Lotto> lottos = makeRandomLottos(purchaseRecord);
 
-        printResultMessage(results, purchaseRecord);
+        AnswerLotto answerLotto = makeAnswerLotto();
+
+        calculateResult(purchaseRecord, lottos, answerLotto);
     }
 
-    private void printResultMessage(Result results, PurchaseRecord purchaseRecord) {
+    private void calculateResult(PurchaseRecord purchaseRecord, List<Lotto> lottos, AnswerLotto answerLotto) {
+        Result results = lottoService.getResult(lottos, answerLotto, purchaseRecord);
         output.print(messageGenerator.getResultMessage(results));
-        output.print(messageGenerator.getProfitMessage(Calculator.calculateProfit(results, purchaseRecord.getUsedMoney())));
+        output.print(messageGenerator.getProfitMessage(results.getReturnRate()));
     }
 
-    private AnswerLotto getAnswerLotto() {
+    private AnswerLotto makeAnswerLotto() {
         output.print(messageGenerator.getAnswerNumberMessage());
         List<Integer> answerNumbers = input.readAnswerNumbers();
         output.print(messageGenerator.getBonusNumberMessage());
         Integer bonusNumber = input.readInteger();
-        return new AnswerLotto(answerNumbers, bonusNumber);
+        return lottoService.getAnswerLotto(answerNumbers, bonusNumber);
     }
 
-    private PurchaseRecord getPurchaseRecord() {
+    private List<Lotto> makeRandomLottos(PurchaseRecord purchaseRecord) {
+        List<Lotto> lottos = lottoService.getRandomLottos(purchaseRecord);
+        output.print(messageGenerator.getLottoMessage(lottos));
+        return lottos;
+    }
+
+    private PurchaseRecord makePurchaseRecord() {
         output.print(messageGenerator.getInputMessage());
         Integer money = input.readInteger();
-        PurchaseRecord purchaseRecord = PurchaseRecord.getNew(money);
-        output.print(messageGenerator.getLottoCountMessage(purchaseRecord.getCount()));
+        PurchaseRecord purchaseRecord = lottoService.getPurchaseRecord(money);
+        output.print(messageGenerator.getLottoCountMessage(purchaseRecord.getNumberOfLottery()));
         return purchaseRecord;
     }
 }
