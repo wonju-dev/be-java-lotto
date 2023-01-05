fefb3eff61b1ddf338fd731ad0b2edad76a906ad
wonju-dev <wonjulee.dev@gmail.com>
2023-01-05 11:58:17 +0900

    delete: delete files generated during resolving merge conflict

--- src/main/java/kr/codesquad/domain/Calculator.java
+++
@@ -1,7 +0,0 @@
-package kr.codesquad.domain;
-
-public class Calculator {
-    public static Double calculateProfit(Result results, Integer usedMoney) {
-        return (results.getNetProfit() - usedMoney) * 100 / 100.0 / usedMoney * 100;
-    }
-}
